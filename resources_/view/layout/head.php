f47dc46187436681a31d1e34acdedf05a171f64d
Aleksandr Kravchuk <kravchuk.aleksandr@makeup.com.ua>
2023-10-13 13:35:43 +0300

    Deleted unused files.

--- resources_/view/layout/head.php
+++
@@ -1,8 +0,0 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <link rel="stylesheet" href="/resources/css/bootstrap.min.css"/>
-    <link rel="stylesheet" href="/resources/css/all.min.css"/>
-    <link rel="stylesheet" href="/resources/css/index_style.css"/>
-    <title>Phone book</title>
-</head>
--- resources_/view/scripts.php
+++
@@ -1,4 +0,0 @@
-<script src="resources/js/jquery-3.3.1.min.js"></script>
-<script src="resources/js/popper.min.js"></script>
-<script src="resources/js/bootstrap.min.js"></script>
-<script src="resources/js/index.js"></script>
